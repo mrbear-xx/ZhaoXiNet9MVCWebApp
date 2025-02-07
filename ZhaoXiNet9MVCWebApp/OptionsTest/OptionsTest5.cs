c115fef450025bc093e8ec6a9edeef44f2519a40
1402998477 <1402998477@qq.com>
2025-02-07 10:11:37 +0800

    修改OptionsTest5添加一条输出

--- ZhaoXiNet9MVCWebApp/OptionsTest/OptionsTest5.cs
+++ ZhaoXiNet9MVCWebApp/OptionsTest/OptionsTest5.cs
@@ -30,6 +30,7 @@ namespace ZhaoXiNet9MVCWebApp.OptionsTest
             Console.WriteLine($"Name:{customApp?.Name}");
             Console.WriteLine($"StartDate:{customApp?.StartDate}");
             Console.WriteLine($"EndDate:{customApp?.EndDate}");
+            Console.WriteLine("代码定义配置信息完成");
         }
     }
 }
